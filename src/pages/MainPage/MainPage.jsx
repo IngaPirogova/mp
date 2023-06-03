a72b09afde9081017c5ec9699f35f8fc9f942f08
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-06-03 12:07:28 +0300

    sum

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,16 +1,18 @@
 import { Link } from 'react-router-dom';
+// import css from './MainPage.module.css';
 
 export const MainPage = () => {
     return (
       <main>
         <section class="auth">
-        <img src="../images/goose2.png" alt="Goose" width="142" height="142"/>
+        {/* <img src='https://cdn-icons-png.flaticon.com/512/622/622669.png' alt='' width={142} height={142}/> */}
+        <img src='https://github.com/IngaPirogova/mp/blob/main/src/images/goose2.png' alt='' width={142} height={142}/>
+           
             <h1 class="auth__title">GooseTrack</h1>
             <button class="auth__button" type="button">Log in 
             <span class="auth__span">LogIn</span>
             </button>
-            <Link to="#">Sign up</Link>
-            
+            <Link to="#">Sign up</Link>            
 
         </section>
        
