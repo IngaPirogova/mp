0fcb7dad79bcacf2f61a767bdb0ad5c94cc0a2ab
IngaPirogova <ingeborga.pirogova@gmail.com>
2026-04-19 13:39:56 +0300

    Summary

--- src/index.js
+++ src/index.js
@@ -4,24 +4,12 @@ import { App } from 'components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
-import { Provider } from 'react-redux';
-import { store} from './redux/store';
-//import {  persistor } from './redux/store';
-
-// import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+  <React.StrictMode>    
       <BrowserRouter >
         <App />
-      </BrowserRouter>
-      {/* </PersistGate> */}
-    </Provider>
+      </BrowserRouter>      
   </React.StrictMode>
 );
 
-//  <BrowserRouter basename='/goit-react-hw-05-movies/'>
-
-//<BrowserRouter basename="/mp/">
